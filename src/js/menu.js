b4577c95240f3d83a3b3ecf965610de769689cb4
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2023-06-09 20:40:59 +0200

    Add Site-menu functions

---
+++ src/js/menu.js
@@ -0,0 +1,28 @@
+const siteMenu = () => {
+
+    // Get element from DOM
+    const menuButton = document.querySelector('.menu__button')
+    const menu = document.querySelector('.site-menu')
+    const bookButton = document.querySelector('.site-header__button')
+    const menuItem = document.querySelector('.site-menu__list')
+
+    // Toggle Menu
+    const toggleMenu = () => {
+        menu.classList.toggle('site-menu--visible')
+        menuButton.classList.toggle('menu__button--close')
+        bookButton.classList.toggle('site-header__button--menu-opened')
+    }
+
+    // Close menu
+    const menuClosed = () => {
+        menu.classList.remove('site-menu--visible')
+        menuButton.classList.toggle('menu__button--close')
+    }
+
+    // Create Event
+    menuButton.addEventListener('click', toggleMenu)
+    menuItem.addEventListener('click', menuClosed)
+
+}
+
+siteMenu()
