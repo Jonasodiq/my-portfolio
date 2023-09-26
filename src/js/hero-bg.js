8058d5d44d7fe3bd138374ecd5e5428092251ef3
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2023-09-26 21:55:43 +0200

    Add text animated

--- src/js/hero-bg.js
+++ src/js/hero-bg.js
@@ -2,3 +2,52 @@ let parent = document.querySelectorAll('.hero__animate-text');
 for(let i = 0; i < parent.length; i++) {
   parent[i].style.width = parent[i].children[0].clientWidth + "px"; 
 };
+
+
+
+let words = ['som skapar användarvänliga och innovativa webbplatser.', 'som älskar att skapa användarvänliga webbplatser', 'Som en passionerad och kreativ designutvecklare', 'där jag formar fantastiska digitala världar', 'med en unik blandning av skicklighet och lidelse'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+let wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
